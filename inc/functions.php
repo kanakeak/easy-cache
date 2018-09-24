c793944b3e8c38f45866fb45b7611fbf270fa1d5
Eyasir Arafat <kanakeak@gmail.com>
2018-09-24 16:34:17 +0600

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -2,7 +2,7 @@
 /**
  * Utility functions for plugin
  *
- * @package  simple-cache
+ * @package  easy-cache
  */
 
 /**
@@ -10,7 +10,7 @@
  *
  * @since  1.4
  */
-function sc_cache_flush() {
+function we_cache_flush() {
 	global $wp_filesystem;
 
 	require_once( ABSPATH . 'wp-admin/includes/file.php' );
